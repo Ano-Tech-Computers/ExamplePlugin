c7330419deb7a6b684666371d77e06e016e779bd
Floyd <floyd@atc.no>
2015-04-07 20:55:50 +0200

    Added example code to show event handlers and log messages

--- src/no/atc/osvein/bukkit/example/ExamplePlugin.java
+++ src/no/atc/osvein/bukkit/example/ExamplePlugin.java
@@ -1,18 +1,59 @@
 package no.atc.osvein.bukkit.example;
 
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ExamplePlugin extends JavaPlugin
+public class ExamplePlugin extends JavaPlugin implements Listener
 {
     @Override
     public void onEnable()
     {
+        // Anything that needs to be done when starting up goes here 
+        // ...
+    	
+    	// Register event handlers
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
+        
+        // Log message example
+        getLogger().info("ExamplePlugin ready");
         
     }
     
     @Override
     public void onDisable()
     {
-        
+
+    	// Anything that needs to be done when shutting down goes here 
+        // ...
+    
+    }
+    
+    // Event handler example
+    @EventHandler
+    public boolean onLogin( PlayerLoginEvent event ) {
+        Player player = event.getPlayer();
+        getLogger().info("Player '"+player.getName()+"' logged in");
+    	return true; // Some event handlers must return a boolean
+    }
+
+    // Another event handler example
+    @EventHandler
+    public void onInteract( PlayerInteractEvent event ) {
+        if (event.hasBlock()) {
+            Player player = event.getPlayer();
+            String pname = player.getName();
+	    	Block block = event.getClickedBlock();
+	    	World world = block.getWorld();
+        }
+        return; // Other event handlers should return nothing
     }
 }
+
